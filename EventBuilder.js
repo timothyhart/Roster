798c5e67d2f13d4cf6ed89718de35a269e3b1f5d
Tim <timothy.hart@my.jcu.edu.au>
2017-05-04 17:22:25 +1000

    Complete before server shit

--- EventBuilder.js
+++ EventBuilder.js
@@ -2,7 +2,31 @@ var parser = require('./CsvParser.js');
 var fs = require('fs');
 var parse = require('csv-parse');
 var moment = require('moment');
+var express = require('express');
+var app = express();
 
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.send(`<html>
+  <head>
+  <link rel='stylesheet' href='fullcalendar.css' />
+  <script src='jquery.min.js'></script>
+  <script src='moment.min.js'></script>
+  <script src='fullcalendar.js'></script>
+  <script src='webCal.js'></script>
+  </head>
+  <body>
+  <div id='cal'></div>
+  </body>
+
+
+  </html>`);
+})
+
+app.listen(1337, () => {
+  console.log("All of the dicks");
+})
 var processData = function(err, output, callback){
   if (err){
     return console.log(err);
@@ -48,7 +72,7 @@ var buildEvents = function(months, times){
   for (var i = 0; i < times.length; i++){
     if (currentProcDate < parseInt(times[i][0]) && !useSecondMonth){
       events.push(BuildEvent(months[0], times[i][0],times[i][1],times[i][2],times[i][3]));
-    } else if (currentProcDate > parseInt(times[i][0])){
+    } else if (currentProcDate > parseInt(times[i][0]) || useSecondMonth){
       useSecondMonth = true;
       events.push(BuildEvent(months[1], times[i][0],times[i][1],times[i][2],times[i][3]));
     }
@@ -62,12 +86,28 @@ var BuildEvent = function(month, dayNumber, startTime, endTime, clinic){
   var startDateTime = dateTimeBuilder(month, dayNumber, startTime);
   var endDateTime = dateTimeBuilder(month, dayNumber, endTime);
 
-
-  var event = {
-    title: clinic,
-    start: startDateTime,
-    end: endDateTime
+  if (endTime < "18:00"){
+    var event = {
+      title: clinic,
+      start: startDateTime,
+      end: endDateTime,
+      color: 'green'
+    }
+  } else if (clinic.indexOf("OC") !== -1){
+    var event = {
+      title: clinic,
+      start: startDateTime,
+      end: endDateTime,
+      color: 'red'
+    }
+  } else {
+    var event = {
+      title: clinic,
+      start: startDateTime,
+      end: endDateTime
+    }
   }
+
 
   return event;
 }
